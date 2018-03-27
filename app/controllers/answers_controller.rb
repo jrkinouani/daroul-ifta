d594be6e324c7b136c35e5dd34018f288fb94345
Oualid JOUHRI <oualidjouhri@Oualids-MacBook-Pro.local>
2018-03-27 14:38:10 +0200

    add notifier

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,6 +1,7 @@
 class AnswersController < ApplicationController
   def create
     @answer = Answer.create(answer_params)
+    AdminMailer.new_answer(@answer, @answer.question).deliver_later
     redirect_back(fallback_location: root_path)
   end
 
