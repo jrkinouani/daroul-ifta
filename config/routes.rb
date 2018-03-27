5c043c86d5b809d0f5d129ff0709e23a1c31ddf4
jrkinouani <kinouani@gmail.com>
2018-03-27 16:50:52 +0400

    creation du form search

--- config/routes.rb
+++ config/routes.rb
@@ -16,5 +16,6 @@ Rails.application.routes.draw do
   get "/apropos" => "static_pages#apropos"
   get "/category" => "static_pages#category"
   get "/faq" => "static_pages#faq"
+  get "/search" => "static_pages#search"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
