de5da419879d4e3c9d5e0cd8a542c337731f16cd
jrkinouani <kinouani@gmail.com>
2018-07-09 12:41:55 +0400

    correction envoi mail admin and change sujet mail

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -4,19 +4,19 @@ class AdminMailer < ApplicationMailer
     @emails = Admin.pluck :email
     @question = question
     @answer = answer
-    mail(to: @emails, subject: 'New answers')
+    mail(to: @emails, subject: 'Nouvelle réponse')
   end
 
   def new_validation(answer, admin)
     @emails = Admin.pluck :email
     @answer = answer
     @admin = admin
-    mail(to: @emails, subject: 'New validation')
+    mail(to: @emails, subject: 'Nouvelle validation')
   end
 
   def new_public_question(question)
     @emails = Admin.pluck :email
     @question = question
-    mail(to: @emails, subject: 'New validation')
+    mail(to: @emails, subject: 'Nouvelle question')
   end
 end
