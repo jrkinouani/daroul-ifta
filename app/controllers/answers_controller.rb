adc1e0ab7ebd53b4e6eb0b8f39f829e22b8ff564
Oualid <jouhri.oualid@gmail.com>
2018-07-12 23:49:34 +0200

    fix new answer mail

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -2,7 +2,7 @@ class AnswersController < ApplicationController
   def create
     @answer = Answer.create(answer_params)
     AdminMailer.new_answer(@answer.question, @answer).deliver_later
-    UserMailer.new_answer(@answer.question, @answer, @question.email ).deliver_later
+    UserMailer.new_answer(@answer.question, @answer, @answer.question.email ).deliver_later
     redirect_back(fallback_location: root_path)
   end
 
