a3a9d144dd5ae9f2d390335cb5580429a4a8b57a
Oualid <jouhri.oualid@gmail.com>
2018-07-13 00:15:48 +0200

    fix new answer mail

--- app/controllers/validations_controller.rb
+++ app/controllers/validations_controller.rb
@@ -3,7 +3,10 @@ class ValidationsController < ApplicationController
   def create
     @validation = Validation.find_or_create(params[:admin_id], params[:answer_id])
     AdminMailer.new_validation(@validation.answer, @validation.admin).deliver_later
-    AdminMailer.new_public_question(@validation.answer.question).deliver_later if @validation.answer.display?
+    if @validation.answer.display?
+      AdminMailer.new_public_question(@validation.answer.question).deliver_later
+      UserMailer.new_answer(@validation.answer.question, @validation.answer, @validation.answer.question.email).deliver_later
+    end
     redirect_back(fallback_location: root_path)
   end
 
