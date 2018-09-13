9a4b676646a2306ef0a7092c49c2494f84090eb4
jrkinouani <kinouani@gmail.com>
2018-09-13 15:28:10 +0400

    page error dropmenu stand by

--- config/routes.rb
+++ config/routes.rb
@@ -30,5 +30,8 @@ Rails.application.routes.draw do
   get '/category' => 'static_pages#category'
   get '/faq' => 'static_pages#faq'
   get '/search' => 'static_pages#search'
+
+  match '/404' => 'errors#error404', via: [:get, :post, :patch, :delete]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
